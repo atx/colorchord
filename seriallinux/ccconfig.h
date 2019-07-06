9766a7e33d128cd4670a93c2bf420b5061a965a1
Josef Gajdusek <atx@atx.name>
2019-07-07 01:28:55 +0200

    Add USB-to-WS2812B forwarder

---
+++ seriallinux/ccconfig.h
@@ -0,0 +1,11 @@
+#ifndef _CCCONFIG_H
+#define _CCCONFIG_H
+
+#define CCEMBEDDED
+#define NUM_LIN_LEDS 86
+#define USE_NUM_LIN_LEDS 86
+#define DFREQ 16000
+
+
+#endif
+
---
+++ seriallinux/embeddedcc.c
@@ -0,0 +1,117 @@
+//
+// This is the teststrap for the Embedded ColorChord System.
+// It is intended as a minimal scaffolding for testing Embedded ColorChord.
+//
+//
+// Copyright 2015 <>< Charles Lohr, See LICENSE file for license info.
+
+
+#include "embeddednf.h"
+#include <assert.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>   // Added by [olel] for atoi
+#include <stdbool.h>
+#include "embeddedout.h"
+
+struct sockaddr_in servaddr;
+int sock;
+
+#define expected_lights NUM_LIN_LEDS
+
+static FILE *fout;
+
+void NewFrame()
+{
+	// A single bit is ~450ns
+	uint8_t buffer[2048];
+	memset(buffer, 0, sizeof(buffer));
+	int i = 0;
+	int bit_i = 0;
+
+	void push_bit(bool bit) {
+		if (bit_i == 8) {
+			bit_i = 0;
+			i++;
+			assert(i < sizeof(buffer) - 1);
+		}
+		if (bit) {
+			buffer[i] |= 1 << bit_i;
+		}
+		bit_i++;
+	}
+
+	void push_ws_bit(bool bit) {
+		if (bit) {
+			push_bit(1);
+			push_bit(1);
+			push_bit(0);
+		} else {
+			push_bit(1);
+			push_bit(0);
+			push_bit(0);
+		}
+	}
+
+	void push_ws_byte(uint8_t byte) {
+		for (int i = 7; i >= 0; i--) {
+			push_ws_bit(!!(byte & (1 << i)));
+		}
+	}
+
+	// Formatted WS2812B bytes
+	for (int i = 0; i < expected_lights; i++) {
+		// Is this the correct order for GRB?
+		push_ws_byte(ledOut[i*3+0]);
+		push_ws_byte(ledOut[i*3+1]);
+		push_ws_byte(ledOut[i*3+2]);
+	}
+
+	// Write the buffer
+	fwrite(buffer, 1, i, fout);
+	// Control character
+	fputc(0xff, fout);
+	fflush(fout);
+
+	HandleFrameInfo();
+	UpdateLinearLEDs();
+}
+
+
+int main( int argc, char ** argv )
+{
+	int wf = 0;
+	int ci;
+
+	if (argc < 2) {
+		fprintf(stderr, "Error: usage: <outfile>\n");
+		return -1;
+	}
+
+	fout = fopen(argv[1], "w");
+	if (fout == NULL) {
+		fprintf(stderr, "Failed to open file '%s'\n", argv[1]);
+	}
+
+	InitColorChord();  // Changed by [olel] cause this was changed in embeddednf
+
+	while( ( ci = getchar() ) != EOF )
+	{
+		int cs = ci - 0x80;
+#ifdef USE_32DFT
+		PushSample32( ((int8_t)cs)*32 );
+#else
+		Push8BitIntegerSkippy( (int8_t)cs );
+#endif
+		wf++;
+		if( wf == 128 )
+		{
+			NewFrame();
+			wf = 0; 
+		}
+	}
+	return 0;
+}
+
